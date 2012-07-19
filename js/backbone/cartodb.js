b9b4474c30821ccc48e10e4fc47c76aae52fd84b
Aaron Steele <eightysteele@gmail.com>
2012-07-18 20:19:47 -0700

    Remove old files.

--- js/backbone/cartodb.js
+++
@@ -1,3 +0,0 @@
-var CartoDB = Backbone.CartoDB({
-  user: 'ogp'
-});
--- js/backbone/collections/challenges.js
+++
@@ -1,8 +0,0 @@
-var ChallengesCollection = CartoDB.CartoDBCollection.extend({
-  sql: function(){
-    return ' \
-      SELECT DISTINCT g.cartodb_id, g.name \
-      FROM ogp_grandchallenges g \
-			ORDER BY g.name';
-  }
-});
